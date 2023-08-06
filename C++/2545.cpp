bbabbc46f538319b068c83de2d88d1d216fc067e
Shaun <shaun@Shauns-MacBook-Air.local>
2023-08-06 21:18:21 +0530

    sort-the-students-by-their-kth-score

---
+++ C++/2545.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
+        int n = score.size();
+        int m = score[0].size();
+        map<int,int> mp;
+        vector<vector<int>> ans;
+        for(int i=0;i<n;i++){
+            mp[score[i][k]] = i;
+        }
+        for(auto i:mp){
+            ans.push_back(score[i.second]);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
