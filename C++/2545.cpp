14c6bbf73c38091ed5b34a59851d19adeb4f3340
Shaun <shaun@Shauns-MacBook-Air.local>
2023-08-06 21:27:06 +0530

    Add imports

--- C++/2545.cpp
+++ C++/2545.cpp
@@ -1,3 +1,8 @@
+#include<vector>
+#include<algorithm>
+#include<iostream>
+#include<map>
+using namespace std;
 class Solution {
 public:
     vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
