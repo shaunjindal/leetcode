d3a8501fe59b788c487ae07d1d0e90cecae8108b
Shaun <shaun@Shauns-MacBook-Air.local>
2023-08-06 23:11:39 +0530

    find-xor-beauty-of-array/description

---
+++ C++/2527.cpp
@@ -0,0 +1,12 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int xorBeauty(vector<int>& nums) {
+        int ans=0;
+        for(auto i:nums){
+            ans^=i;
+        }
+        return ans;
+    }
+};
