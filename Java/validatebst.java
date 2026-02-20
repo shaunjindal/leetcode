de867dd1d55171e62250512042a7570c4778b967
Shaun <shaun@Shauns-MacBook-Air.local>
2026-02-20 12:59:30 +0530

    validate-binary-search-tree

---
+++ Java/validatebst.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBSTHelper(root, Long.MAX_VALUE, Long.MIN_VALUE);
+    }
+
+    public boolean isValidBSTHelper(TreeNode root, long max, long min) {
+        if (root == null) return true;
+        if (root.val > min && root.val < max) {
+            return isValidBSTHelper(root.left, root.val, min) && isValidBSTHelper(root.right, max, root.val);
+        }
+        return false;
+    }
+}
